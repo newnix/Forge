cbb4c59bf5eb5e8f46a5d76a57df176642da298a
deux3x@protonmail.ch <deux3x@protonmail.ch>
2015-11-20 05:17:53 +0000

    minor changes to files

--- cpp/src/auxil.cpp
+++ cpp/src/auxil.cpp
@@ -5,7 +5,7 @@
 #include "budgetv3.h"
 
 /*
- * Definition of the function mainOffice.
+ * Definition of the function auxOffice.
  * This function is declared a friend by the Budget class.
  * It adds the value of the argument, 'b' to the static corpBudget.
  */
@@ -14,4 +14,14 @@ void auxOffice::addBudget(double b, Budget &div)
 {
     auxBudget += b;
     div.corpBudget += b;
+}
+
+/*
+ * Adding a definition to the function mainOffice since one isn't included in the book
+ * Will simlpy add the value 'b' to the corpBudget static var.
+ */
+
+void Budget::mainOffice(double b)
+{
+    corpBudget += b;
 }
