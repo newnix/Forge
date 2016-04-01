492f3b02aab0fc64973eb1b96e8ab9bf3d8fcfd9
deux3x@protonmail.ch <deux3x@protonmail.ch>
2016-04-01 14:03:38 +0000

    started c

---
+++ c/cthw/exercise01/ex1.c
@@ -0,0 +1,9 @@
+// Add in the stdio header
+#include <stdio.h>
+
+// First example program for learning c the hard way
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    return 0;
+}
---
+++ c/cthw/exercise03/ex3.c
@@ -0,0 +1,15 @@
+// Creating the third example. 
+// Will comment anything that was added to fix error messages
+
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
