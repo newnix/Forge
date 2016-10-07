43c58e66721f7009127e337c05f3a390933b10ab
deux3x@gmail.com <deux3x@gmail.com>
2016-10-07 22:51:01 +0000

    made a bit more progress on the exercises

--- c/kr/s1/ex118.c
+++ c/kr/s1/ex118.c
@@ -7,7 +7,7 @@
 
 /* forward declarations */
 int get_line(char s[], int lim); /* get a line from stdin */
-char[] condense(char s[], int a, int b, int c); /* condense whitespace */
+char[] condense(char s[]); /* condense whitespace */
 void copy(to[], from[]); /* copy one string into another */
 
 int main()
@@ -24,3 +24,38 @@ int main()
 	/* program ends if it reaches EOF or len == 0 */
 	return 0;
 }
+
+int get_line(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+	{	
+		s[i] = c;
+	}
+	if (c == '\n')
+	{
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(to[], from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
+
+char[] condense(char s[])
+{
+	int a, b, c;
+
+	for (a = 0; a < MAX - 1 && (s[a] != '\0'); ++a)
+	{
+		b = a++;
+		if (s[a] == '\t' || s[a] == ' ') && 
