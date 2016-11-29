3b91746c67cb0a8a40db8d5c6b9d4048b90705f4
deux3x@gmail.com <deux3x@gmail.com>
2016-11-29 23:00:27 +0000

    Began work on detab.c, foundation is there, will just need to finish the expand_tab() function and test to ensure it works.

--- c/kr/s1/detab.c
+++ c/kr/s1/detab.c
@@ -2,5 +2,66 @@
  * Exercise 1-20 in K&R's C
  * page 34
  * meant to replace tabs with the relevant number of spaces
+ * manipulates strings pulled from stdin rather than a file
  */
 
+#include <stdio.h>
+#define SPACES 4 /* how many spaces should a tab be replaced with */
+#define MAX 4000 /* longest string allowed */
+
+int get_line(char s[], int lim); /* pull a line from stdin*/
+void expand_tab(char in[], char out[]); /* used to swap tabs for spaces using the SPACES value */
+void copy(char to[], char from[]); /* used to manipulate strings with more flexibility, may not really be needed in this practice program */
+
+int main()
+{
+	/* first things first, let's get some vars */
+	char instr[MAX]; /* input string */
+	char tabstr[MAX]; /* tab string */
+	int len; /* hold the length of the string from stdin, in case keeping such a thing becomes useful */
+
+	/* actually get the input from the user */
+	while ((len = get_line(instr, MAX)) > 0)
+	{
+		/* expand the tabs */
+		expand_tab(instr, tabstr);
+		/* there probably needs to be a check made to count the tabs and determine how
+		 * much extra space in the array needs to be available to expand all the tabs,
+		 * and throw an error should the projected length of tabstr not be small enough to fit with MAX
+		 */
+	return 0;
+}
+
+int get_line(char s[], int lim)
+{
+	int c, i;
+
+	for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+	{
+		s[i] = c;
+	}
+	if (c == '\n')
+	{
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(char to[], char from[])
+{
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
+
+void expand_tab(char in[], char out[])
+{
+	/* use a for loop to find each instance of a '\t',
+	 * then pause inside of one loop to substitute 4 spaces instead,
+	 * otherwise, the content of in[] should not be modified.
+	 */
+	int a,b; /* counters for each string */
+}
