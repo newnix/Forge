a33c1c83b0cdd1ce4195880e66e20661ff15a358
deux3x@gmail.com <deux3x@gmail.com>
2016-12-06 16:36:24 +0000

    clearing up any current .swp files

--- c/kr/s1/entab.c
+++ c/kr/s1/entab.c
@@ -3,3 +3,19 @@
  * page 34
  * Meant to replace runs of spaces with tabs
  */
+
+#include <stdio.h>
+#define SPACES 4 /* tabstop value, should there be 4 spaces consecutively, replace them with a single tab */
+#define MAX 4000 /* longest string allowed */
+
+int get_line(char s[], int lim); /* pull a line from stdin */
+void entabulate(char in[], char out[]); /* add tabs and such */
+
+int main()
+{
+	/* get the vars in there */
+	char instr[MAX]; /* input string */
+	char outstr[MAX]; /* output string */
+	int len; /* just in case I need to compare lengths of things */
+
+	/* get a string from the user */
