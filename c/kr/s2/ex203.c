82bd341d30150140d7677a809fcc0943441698ac
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-01-11 01:23:00 +0000

    found some broken switches, program runs correctly now

--- c/kr/s2/ex203.c
+++ c/kr/s2/ex203.c
@@ -19,7 +19,6 @@ char upper(int c); /* convert lower char to upper */
 int main()
 {
 	char hexin[max]; /* string containing the hex value */
-	int hex;	/* integer value of the hex string */
 	int len;
 	
 	printf("Enter a hexadecimal value, with or without the leading \"0x\":\n");
@@ -28,7 +27,7 @@ int main()
 	{
 		/* not sure what to put here yet */
 		printf("%s\n",hexin);
-		printf("Is %.2f when converted to an integer.\n",htoi(hexin,len));
+		printf("Is %.0f when converted to an integer.\n",htoi(hexin,len));
 	}
 
 	return 0;
@@ -57,38 +56,42 @@ double htoi(char s[], int len)
 	int i; /* used to step through the string, checking for invalid values */
 	int h; /* holds the value of hex[] so we have a valid hex string */
 	char hex[max]; /* verified hex string */
-
+	int hlen; /* separate the length of the string from the conversion string */
+	
+	h = 0;
 	/* switch for the possibility of a preceeding '0x' */
 	if ((s[0] == '0') && (upper(s[1]) == 'X'))
 	{
+		hlen = len - 2;
 		/* start i at 2 */
-		for (i = 2; i < max -1; i++)
+		for (i = 2; i < max - 1; i++)
 		{
 			/* test for the int values for various valid characters */
-			if ((upper(s[i]) >= 'A' && upper(s[i]) <= 'F') || (s[i] >='0' && s[i] <= '9'))
+			if ((upper(s[i]) >= 'A' && upper(s[i]) <= 'F') || (s[i] >= 48 && s[i] <= 57))
 			{
 				/* collect the values so we can turn them into an int later */
 				hex[h] = s[i]; 
 				h++;
 			}
-		} len =- 2;
+		} hex[h + 1] = '\0';
 	}
 	else
 	{
+		hlen = len;
 			/* start i at 0 */
-		for (i = 0; i < max -1; i++)
+		for (i = 0; i < max - 1; i++)
 		{
 			/* test for the int values for various valid characters */
-			if ((upper(s[i]) >= 'A' && upper(s[i]) <= 'F') || (s[i] >='0' && s[i] <= '9'))
+			if ((upper(s[i]) >= 'A' && upper(s[i]) <= 'F') || (s[i] >= 48 && s[i] <= 57))
 			{
 				/* collect the values so we can turn them into an int later */
 				hex[h] = s[i];
 				h++;
 			}
-		}
+		} hex[h + 1] = '\0';
 	/* after verifying it's good, convert to an integer value */
 	}
-	return hexcon(hex,len);
+	return hexcon(hex,hlen);
 }
 
 /*
@@ -114,12 +117,13 @@ double hexcon(char h[], int len)
   	
 	double dec; /* the decimal value of the conversion process so far */
   	int i; /* for running through the string */
-	int hexval[len-1], hexpow[len-1]; /* should be the right size arrays for the value and power of each character to convert */
+	double hexval[len-1], hexpow[len-1]; /* should be the right size arrays for the value and power of each character to convert */
   /* calculate the values independantly, then return the calculated int value */
 	
 	dec = 0;
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len - 1; i++)
 	{
+/*		printf("h[%d] = %c, %c\n",i, h[i], upper(h[i])); */
 		/* determine the value of each character in the string */
 		/* should be a switch/case block, since we're basically getting chars to look at */
 		switch(upper(h[i]))
@@ -127,31 +131,79 @@ double hexcon(char h[], int len)
 			case 'A' :
 			/* put the value of the position into an array, along with the exponent to use for it */
 				hexval[i] = 10;
-				hexpow[i] = len - (i + 1);
+				hexpow[i] = len - (i + 2);
+				break;
 			case 'B' :
 				hexval[i] = 11;
-				hexpow[i] = len - (i + 1);
+				hexpow[i] = len - (i + 2);
+				break;
 			case 'C' :
 				hexval[i] = 12;
-				hexpow[i] = len - (i + 1);
+				hexpow[i] = len - (i + 2);
+				break;
 			case 'D' :
 				hexval[i] = 13;
-				hexpow[i] = len - (i + 1);
+				hexpow[i] = len - (i + 2);
+				break;
 			case 'E' :
 				hexval[i] = 14;
-				hexpow[i] = len - (i + 1);
+				hexpow[i] = len - (i + 2);
+				break;
 			case 'F' :
 				hexval[i] = 15;
-				hexpow[i] = len - (i + 1);
+				hexpow[i] = len - (i + 2);
+				break;
+			case '1' :
+				hexval[i] = 1;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '2' :
+				hexval[i] = 2;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '3' :
+				hexval[i] = 3;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '4' :
+				hexval[i] = 4;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '5' :
+				hexval[i] = 5;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '6' :
+				hexval[i] = 6;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '7' :
+				hexval[i] = 7;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '8' :
+				hexval[i] = 8;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '9' :
+				hexval[i] = 9;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '0' :
+				hexval[i] = 0;
+				hexpow[i] = len - (i + 2);
+				break;
+			case '\0':
+				break;
 			default :
-				hexval[i] = h[i];
-				hexpow[i] = len - (i + 1);
+				break;
 		}
 	}
 	/* so now we need math.h */
-	for (i = 0; i < len; ++i)
+	for (i = 0; i < len - 1; ++i)
 	{
-		dec =+  pow((double) hexval[i], (double) hexpow[i]);
+/*		printf("hexval[%d] = %f\nhexpow[%d] = %f\n",i,hexval[i],i,hexpow[i]); */
+		dec = dec +  ( (double) hexval[i] * pow(16, (double) hexpow[i]));
 	}
 
 	return dec;
@@ -161,7 +213,7 @@ double hexcon(char h[], int len)
 char upper(int c)
 {
 	int upperdiff; /* value between upper and lower letters */
-	upperdiff = (97 - 65); /* difference between 'a' and 'A' */
+	upperdiff = 97 - 65; /* difference between 'a' and 'A' */
 
 	if ((c <= 122) && (c >= 97)) /* between lower 'z' and 'a' */
 		c = (c - upperdiff);
