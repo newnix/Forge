19d7d3a19abb75b4f96b97aa774326c137c0ad99
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-01-13 21:44:46 +0000

    ex122.c has functioning basic word-wrap, still needs word detection though

--- c/kr/s1/ex122.c
+++ c/kr/s1/ex122.c
@@ -4,10 +4,10 @@
  * Meant to act as rudimentary word-wrap
  */
 
-/* wrap is the minimum number of characters at which to wrap words */
-#define wrap 70
-#define max 1000 /* max string length */
 #include <stdio.h>
+/* wrap is the minimum number of characters at which to wrap words */
+#define WRAP 70
+#define MAX 1000 
 
 /* forward declaration of some functions */
 int getline(char s[], int max);
@@ -16,18 +16,19 @@ void wrapit(char s[], int len, int wlen, char w[]);
 int main()
 {
 	int len; /* length of the current string */
-	char instr[max]; /* the input string */
-	char wstr[max * 2]; /* the wrapped string, should be long enough to catch any string passed to instr */
+	char instr[MAX]; /* the input string */
+	char wstr[MAX * 2]; /* the wrapped string, should be long enough to catch any string passed to instr */
 	len = 0;
 
-	while ((len = getline(instr, max)) > 0)
+	while ((len = getline(instr, MAX)) > 0)
 	{
 		/* wrap instr as best as possible */
-		wrapit(instr, len, wrap, wstr);
-		printf("%s\n", wrstr);
+		wrapit(instr, len, WRAP, wstr);
+		printf("%s\n", wstr);
 		/* let the user know that they can either type more or hit ^D */
 		printf("\nEither enter more text to pass through word-wrap or hit ^D to exit:\n");
 	}
+	return 0;
 }
 
 int getline(char s[], int max)
@@ -50,7 +51,25 @@ int getline(char s[], int max)
 void wrapit(char s[], int len, int wlen, char w[])
 {
 	/* wrap the string as close to wlen as possible */
-	bool wrap; /* are we currently trying to wrap the string? */
+	int wrap; /* are we currently trying to wrap the string? psuedo-boolean for ansi c */
 	int i; /* current spot in the input string */
 	int wi; /* current spot in the wrapped string */
+
+	for (wrap = wi = i = 0; i < len; ++i)
+	{
+		if (wrap >= WRAP)
+		{
+			w[wi] = '\n';
+			wi++;
+			w[wi] = s[i]; /* this will be a mostly 1-1 mapping */
+			wi++; /* ensure that wi is keeping up with i */
+			wrap = 0; /* reset the counter */
+		}
+		else
+		{
+			w[wi] = s[i]; /* this will be a mostly 1-1 mapping */
+			wi++; /* ensure that wi is keeping up with i */
+			wrap++;
+		}
+	}
 }
