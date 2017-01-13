4e452d20b5405d940f20b42bc8f572995797efdd
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-01-13 01:24:55 +0000

    added header file, progressed on ex122

--- c/kr/s1/ex122.c
+++ c/kr/s1/ex122.c
@@ -11,11 +11,23 @@
 
 /* forward declaration of some functions */
 int getline(char s[], int max);
-void wrapit(char s[], int wlen);
+void wrapit(char s[], int len, int wlen, char w[]);
 
 int main()
 {
- 	/* do stuff */
+	int len; /* length of the current string */
+	char instr[max]; /* the input string */
+	char wstr[max * 2]; /* the wrapped string, should be long enough to catch any string passed to instr */
+	len = 0;
+
+	while ((len = getline(instr, max)) > 0)
+	{
+		/* wrap instr as best as possible */
+		wrapit(instr, len, wrap, wstr);
+		printf("%s\n", wrstr);
+		/* let the user know that they can either type more or hit ^D */
+		printf("\nEither enter more text to pass through word-wrap or hit ^D to exit:\n");
+	}
 }
 
 int getline(char s[], int max)
@@ -35,7 +47,10 @@ int getline(char s[], int max)
 	return i;
 }
 
-void wrapit(char s[], int wlen)
+void wrapit(char s[], int len, int wlen, char w[])
 {
 	/* wrap the string as close to wlen as possible */
+	bool wrap; /* are we currently trying to wrap the string? */
+	int i; /* current spot in the input string */
+	int wi; /* current spot in the wrapped string */
 }
