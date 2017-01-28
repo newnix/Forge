c5f911be599536a0884e5d2a5b22a76712d84f9e
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-01-28 21:08:44 +0000

    started practice with 2d arrays

---
+++ c/kr/s1/2darray.c
@@ -0,0 +1,34 @@
+/* 
+ * This is a practice program to assign, write to, and read from a 2D array
+ */
+
+#include <stdio.h>
+#include "../headers/get_line.h"
+
+#define MAX 10
+
+int main()
+{
+	/* first we need to set up the array(s) */
+	int d[10][MAX]; /* hold up to 10 MAX length strings */
+	int x,y; /* length of the input string */
+
+	/* somehow grab the stuff from the user and stuff it in the array */
+	for (x = 0; x < 10; x++)
+	{
+		/* and another, nested loop */
+		for (y = 0; y < MAX; y++)
+		{
+			d[x][y] = y; /* basically just store where we are right now */
+		}
+	}
+	/* do it again, but this time print the stuff out */
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < MAX; y++)
+		{
+			printf("d[%d][%d] = %d\n",x,y,d[x][y]); /* according to valgrind, there's a leak here */
+		}
+	}
+	return 0;
+}
--- c/kr/s1/ex123.c
+++ c/kr/s1/ex123.c
@@ -9,7 +9,6 @@
 /* I don't have any experience reading/writing files, so I'll settle for stripping comments from stdin */
 #include <stdio.h>
 #include "../headers/get_line.h"
-#include <stdlib.h>
 
 /* creating a custom version of the get_line function to allow it to continue past a single newline */
 int get_line(char s[], int max);
@@ -18,7 +17,7 @@ int main()
 {
 	int len; /* length of the input string given by the user */
 	char instr[MAX]; /* input string pulled from user */
-	char *lines[MAX]; /* intended to hold up to MAX lines of input from instr */
+	char lines[MAX][MAX]; /* intended to hold up to MAX lines of input from instr */
 	int linelen[MAX]; /* separate value to hold the length of each string stored */
 	int linenum; /* current line number we're on */
 	int step; /* iterator */
@@ -28,14 +27,14 @@ int main()
 	while ((len = get_line(instr, MAX)) > 0)
 	{
 		/* grab all the strings and track them in the array lines[] */
-		strcopy(lines[linenum],instr); /* store the current line in lines[linenum] */
+		lines[linenum][0] = instr; /* store the current line in lines[linenum] */
 		linelen[linenum] = len; /* for the same position in linelen[] keep lines[] length */
 		linenum++;
 		printf("instr: %slines[%d]: %s\n", instr, (linenum - 1), lines[linenum - 1]);
 	}
 	/* so now we need to do things, like looping through the arrays and getting rid of comments */
 	/* first, let's prove that I stored the information correctly */
-	for (step = 0; step <= linenum; step++)
+	for (step = 0; step < linenum; step++)
 	{
 		printf("lines[%d]: %s Length:%d\n",step,lines[step],linelen[step]);
 	}
