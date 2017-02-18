949349b7713cb26a7e0a38ee2edf3dad2d20271d
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-02-18 23:01:36 +0000

    tried making some headway with the bitwise operations

--- c/kr/s2/ex206.c
+++ c/kr/s2/ex206.c
@@ -21,9 +21,9 @@
  * This should be evaluated like the following:
  * x rightshifted to p + 1 - n bits
  * AND
- * MAX leftshifted n bits, then flipped again
+ * MAX leftshifted n bits, then flipped again (1111 1111 << 2 = 1111 1100; ~11111100 = 0011)
  * so if x = p = n = 2
- * 2 >> 1 bits AND 1100 = 0001 & 1100 = 0;
+ * 2 >> 1 bits AND 1100 = 0001 & 0011 = 1;
  */
 unsigned int setbits(unsigned int x, unsigned int p, unsigned int n, unsigned int y);
 
@@ -33,7 +33,7 @@ int main()
 	x = 2;
 	p = 1;
 	/* do things */
-	printf("%d\n",setbits(x,p,x,p));
+	printf("%d\n",setbits(x,x,x,x));
 	return 0;
 }
 
@@ -44,10 +44,9 @@ unsigned int setbits(unsigned int x, unsigned int p, unsigned int n, unsigned in
 	 * with the n bits from p 
 	 * set to the rightmost n bits of y
 	 *
-	 * so if x = 1100 0011, n = 2, p = 2, y = 2
-	 * this should return something like 
-	 * 11001100, if I'm right.
+	 * if x = y = p = n = 2;
+	 * ....
 	 */
-	return x & p;
+	return (x >> (p + 1 - n) & ~(~0 << n));
 	/* I know this is wrong, but I want to have something here */
 }
--- c/kr/s2/ex207.c
+++ c/kr/s2/ex207.c
@@ -10,7 +10,30 @@
 #include <stdlib.h>
 #include "../headers/get_line.h"
 
+unsigned int flipbits(unsigned int x, unsigned int p, unsigned int n);
+
 int main()
 {
-	/* do things */
+	unsigned int x, p, n; /* our vars */
+	x = 10;
+	p = 2;
+	n = 3;
+
+	printf("Result is %u\n",flipbits(x,p,n));
+
+	return 0;
+}
+
+unsigned int flipbits(unsigned int x, unsigned int p, unsigned int n)
+{
+	/* 
+	 * Meant to flip the n bits starting at p in x
+	 * EX: x = 10, p = 2, n = 3
+	 * x = 0000 1010
+	 * return 0000 0100 = 4
+	 */
+
+	/* return 4;  hooray for shortcuts! */
+	/* now we try getting a real solution in place */
+	return x & ~(x >> (p + n));
 }
