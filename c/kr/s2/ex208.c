66a3861e295dc18d62e0ada3c3129129d189c303
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-03-01 23:13:36 +0000

    got the rotation excersize worked out with help from wikipedia of all places

--- c/kr/s2/ex208.c
+++ c/kr/s2/ex208.c
@@ -10,7 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "../get_line.h"
+#include "../headers/get_line.h"
 
 unsigned short int rrot(unsigned short int x, unsigned short int n);
 
@@ -26,9 +26,9 @@ int main()
 
 	/* let's get the resulting int first, then print the resulting value */
 	result = rrot(x,n);
-
+	printf("Size of x: %zu, (%lu)\n",sizeof(x), ((sizeof(x) * 8) -1)); 
 	/* now let's show the result in decimal and hex */
-	printf("Right rotation of %hu by %hu bits: %hu\t(%hX)\n",x,n,result,result);
+	printf("Right rotation of %hu (%hX) by %hu bits: %hu\t(%hX)\n",x,x,n,result,result);
 }
 
 unsigned short int rrot(unsigned short int x, unsigned short int n)
@@ -38,9 +38,22 @@ unsigned short int rrot(unsigned short int x, unsigned short int n)
 	 * ex: x = 123456, n = 3
 	 * return 456123, but in binary form.
 	 */
-	unsigned short int r;
+	unsigned short int r, bitlen;
 	r = 0;
-	
+	bitlen = (8 * sizeof(x) - 1); /* this should help maintain compatibility with larger primitives */
+	/* this was taken from the wikipedia page explaining rotations, might need some reworking */
+	/* the recommended method was r = (x << n) | (x >> (-n & bitlen)); */
+	r = (x << (bitlen - n)) | (x >> n);
+	/* 
+	 * 00000000 00001010 rotated to the right by 3 -> 01000000 00000001
+	 * should be a few thousand, with an odd number at the end.
+	 *
+	 * 00000000 00001010 << 3 | 00000000 00001010 >> 13
+	 * 00000000 01010000 | 0
+	 * 00000000 01010000 = 80 (2^6 + 2^4)
+	 *
+	 * accidentally followed the instructions for a left rotation, though this has simplified the rest of the excersize
+	 */
 	
 	return (r);
 }
