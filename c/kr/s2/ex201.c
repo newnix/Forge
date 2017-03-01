96b94d26c636d86ea14137e9a88882d8a8b742ef
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-03-01 05:31:30 +0000

    added more body to the rotation excersize

--- c/kr/s2/ex201.c
+++ c/kr/s2/ex201.c
@@ -31,25 +31,27 @@ int main()
 
 	/* here we'll get the "word size" or bit-length of some of these primitives as well. */
 	/* The following method was shamelessly stolen from https://en.wikipedia.org/wiki/sizeof */
+	short int *shortint = (short int *) malloc(10 * sizeof(short int));
+	printf("\nshort int word size: %hu, %zu\n", shortint, sizeof(short int));
+	free(shortint); /* no need to hold onto it anymore */
+	
 	int *intsize = (int *) malloc(10 * sizeof(int));
-	printf("\nint word size: %u, %zu\n", intsize, sizeof(int));
+	printf("int word size: %u, %zu\n", intsize, sizeof(int));
 	free(intsize); /* no need to hold onto it anymore */
 	
-	short int *shortint = (short int *) malloc(10 * sizeof(short int));
-	printf("short int word size: %zu, %zu\n", shortint, sizeof(short int));
-	free(shortint); /* no need to hold onto it anymore */
 	/* and let's keep going so there's easy reference material in source code and binary form! */
 	long int *lngsize = (long int *) malloc(10 * sizeof(long int));
 	printf("long int word size: %lu, %zu\n", lngsize, sizeof(long int));
 	free(lngsize);
 
+	float *fltsize = (float *) malloc(10 * sizeof(float));
+	printf("float word size: %zu, %zu\n", fltsize, sizeof(float));
+	free(fltsize);
+
 	double *dblsize = (double *) malloc(10 * sizeof(double));
-	printf("double word size: %zu, %zl\n", dblsize, sizeof(double));
+	printf("double word size: %zu, %zu\n", dblsize, sizeof(double));
 	free(dblsize);
 
-	float *fltsize = (float *) malloc(10 * sizeof(float));
-	printf("float word size: %zu, %zl\n", fltsize, sizeof(float));
-	free(fltsize);
 
 	return 0;
 }
--- c/kr/s2/ex208.c
+++ c/kr/s2/ex208.c
@@ -12,7 +12,35 @@
 #include <stdlib.h>
 #include "../get_line.h"
 
+unsigned short int rrot(unsigned short int x, unsigned short int n);
+
 int main()
 {
-	/* do things */
+	/*
+	 * first let's test this on some statically assigned values
+	 */
+
+	unsigned short int x, n, result;
+	x = 10;
+	n = 3;
+
+	/* let's get the resulting int first, then print the resulting value */
+	result = rrot(x,n);
+
+	/* now let's show the result in decimal and hex */
+	printf("Right rotation of %hu by %hu bits: %hu\t(%hX)\n",x,n,result,result);
+}
+
+unsigned short int rrot(unsigned short int x, unsigned short int n)
+{
+	/*
+	 * rotate x to the right by n positions
+	 * ex: x = 123456, n = 3
+	 * return 456123, but in binary form.
+	 */
+	unsigned short int r;
+	r = 0;
+	
+	
+	return (r);
 }
