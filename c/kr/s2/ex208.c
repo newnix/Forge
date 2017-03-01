4dde356a14b5c7d9d99830db79685bf07ffe3c0e
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-03-01 01:26:48 +0000

    minor clarification for ex208.c

--- c/kr/s2/ex208.c
+++ c/kr/s2/ex208.c
@@ -2,7 +2,9 @@
  * Exercise 2-8 in K&R's ANSI C 
  * Meant to create a function called rightrot(x,n)
  * that returns the value of the integer x rotated to the right by n bit positions
- *
+ * so if x = 10, n = 3
+ * 00000000 00001010, 3
+ * return 01000000 00000001
  * hooray for bitwise operations!
  */
 
