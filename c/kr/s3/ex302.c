0909f159a5dcd64743bab291776dd2764c74eb79
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-03-08 01:44:48 +0000

    started on the next exercise

---
+++ c/kr/s3/ex302.c
@@ -0,0 +1,24 @@
+/*
+ * Exercise 3-2 from K&R's ANSI C 
+ * found on page 60
+ * Meant to create a function called escape(s,t)
+ * which converts the newline and tab characters to their escape sequences
+ * and vice-versa while copying string t to s using a switch statement
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/get_line.h"
+
+/* create a function prototype */
+void escape(char s[], char t[]);
+
+int main()
+{
+	/* do things */
+}
+
+void escape(char s[], char t[])
+{
+	/* do more things */
+}
