689b86b11ba7d3806a48ffd60439080c259b1f42
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-03-17 00:33:15 +0000

    atoi now does stuff

--- c/kr/s3/itoa.c
+++ c/kr/s3/itoa.c
@@ -5,16 +5,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <reverse.c>
+#include <string.h>
 
 #define MAX 100 
 
 /* prototype for atoi */
 void itoa(int n, char s[]);
+/* prototype for reverse() */
+void reverse(char s[]);
 
 int main()
 {
-	/* do things */
+	/* create an array to print out */
+	char astring[MAX];
+	/* integer to have been converted */
+	int input;
+
+	input = ~MAX;
+	itoa(input, astring);
+	printf("%d converted to a string is:\n%s\n",input,astring);
 	return 0;
 }
 
@@ -37,4 +46,17 @@ void itoa(int n, char s[])
 		s[i++] = '-';
 	s[i] = '\0';
 	reverse(s);
+}
+
+/* reverse, as found on page 62 */
+void reverse(char s[])
+{
+	int c, i, j;
+
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
 }
