2e4a307968c7ebeb15ad23d3ba1d62775c77910f
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-03-31 00:35:20 +0000

    minor work on ex306.c

--- c/kr/s3/ex306.c
+++ c/kr/s3/ex306.c
@@ -6,6 +6,7 @@
  * is smaller than this, it must be left-padded to fill
  */
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,4 +26,37 @@ int main(void) {
 
 void itoa(int x, char * s, int p) {
 	/* this is where the less boring magic happens */
+	int i, sign;
+	
+	/* initialize the counter */
+	i = 0;
+	/* check for a negative number */
+	if ((sign = n) < 0) {
+		if (n == INT_MIN) {
+			n = -(n + 1);
+			i = -1;
+		}
+		else {
+			n = -n;
+		}
+	}
+	/* now we convert it to a string */
+	do {
+		if (i == -1) {
+			s[i+1] = (n % 10) + 1 + '0'; /* correct for the limitation of a signed integer */
+			i++; /* set i to the correct index */
+		}
+		else {
+			s[i] = (n % 10) + '0'; 
+		}
+		i++;
+	} while ((n /= 10) > 0);
+
+	if (sign < 0) {
+		s[i] = '-'; 
+		s[i+1] = 0;
+	}
+	else {
+		s[i+1] = 0;
+	}
 }
