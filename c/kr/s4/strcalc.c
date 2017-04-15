295f9778a6c8c0dca02431924da869ffb5ebc180
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-04-15 00:35:13 +0000

    started working on functions for strcalc.c

--- c/kr/s4/strcalc.c
+++ c/kr/s4/strcalc.c
@@ -8,12 +8,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* create a static array to list malloc()'d pointers */
+extern char ptrs[1024][1024]; /* I can't imagine having to track more than that at this time */
+
 /* create a function to determine what class characters are */
 void  charclass(char * s);
 /* and a function for the math, similar to atof() */
 double compute(char *s);
 /* and one to actually isolate the arithmetic expressions, no accounting for parens/braces/brackets */
 void isolateMath(char *s);
+/* create a function to create and allocate pointers, while tracking them in an array */
+ptrlist(char * type);
 
 int main(void) {
 	/* do some magic things */
@@ -34,3 +39,28 @@ void charclass(char *s, int slen) {
 	int * op;
 	int * other;
 
+	if ((alpha = malloc(sizeof(int))) != NULL) {
+		prtlist(alpha);
+	}
+	else {
+		printf("ERROR: CAN'T ALLOCATE MEMORY FOR *alpha\n");
+	}
+
+	if ((num = malloc(sizeof(int))) != NULL) {
+		ptrlist(num);
+	}
+	else {
+		printf("ERROR: CAN'T ALLOCATE MEMORY FOR *num\n");
+	}
+
+	if ((op = malloc(sizeof(int))) != NULL) {
+		ptrlist(op);
+	}
+	else {
+		printf("ERROR: CAN'T ALLOCATE MEMORY FOR *op\n");
+	}
+
+	if ((other = malloc(sizeof(int))) != NULL) {
+		ptrlist("ERROR: CAN'T ALLOCATE MEMORY FOR *other\n");
+	}
+}
