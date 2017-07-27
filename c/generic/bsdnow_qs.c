6b6727ce68e93c1d0155a6195a000d61e67b2d4c
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-07-27 20:19:47 +0000

    Added beginnings of Q&A C file sources

---
+++ c/generic/bsdnow_qs.c
@@ -0,0 +1,20 @@
+/*
+ * Semi-serious BSDNow questions sources file,
+ * To ask a few questions of Benedict and Allan.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+#include <ctype.h>
+
+/* ask questions for given host */
+void ask_questions(char * name);
+/* dump the questions to stdout instead */
+void dump_questions(void); 
+
+int
+main(int argc, char *argv[]) { 
+	/* here's where the questions go */
+	return 0;
+}
