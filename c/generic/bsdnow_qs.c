d48dda71cad507430ae825362313bc53b7af181c
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-07-28 03:00:24 +0000

    filled out the rest of the qsuedocode necessary to get the basic format of the question program working

--- c/generic/bsdnow_qs.c
+++ c/generic/bsdnow_qs.c
@@ -9,12 +9,44 @@
 #include <ctype.h>
 
 /* ask questions for given host */
-void ask_questions(char * name);
+void ask_questions(char *name);
 /* dump the questions to stdout instead */
 void dump_questions(void); 
 
 int
 main(int argc, char *argv[]) { 
+	char **allan_questions;
+	char **benedict_questions;
 	/* here's where the questions go */
 	return 0;
+}
+
+void
+ask_questions(char *name){
+	/* run through the questions for the given host */
+	switch upper(*name) {
+		case "ALLAN":
+			for (q = 0; q < numqustions; q++) { 
+				printf("%s\n",*allan_questions[q]);
+			}
+			break;
+		case "BENEDICT":
+			for (q = 0; q < numquestions; q++) { 
+				printf("%s\n",*benedict_questions[q]);
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+void
+dump_questions(void) { 
+	int q;
+	for (q = 0; q < numquestions; q++) { 
+		printf("%s\n",*allan_questions[q]);
+	}
+	for (q = 0; q < numquestions; q++) { 
+		printf("%s\n",*benedict_questions[q]);
+	}
 }
