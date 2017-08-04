5df8a008fa0484e17404299e7288776fc6b6d30a
deux3x@protonmail.ch <deux3x@protonmail.ch>
2017-08-04 13:59:11 +0000

    fopen.c compiles, now just needs to be modified for correct operation

--- c/generic/fopen.c
+++ c/generic/fopen.c
@@ -27,11 +27,11 @@ main(int argc, char *argv[]) {
 				break;
 			case 'h':
 				hflag = 1;
-				(void)fprint(stderr, "usage: %s [-dh]\n",__progname);
-				break;
+				(void)fprintf(stderr, "usage: %s [-dh]\n",__progname);
+				return 0;
 			default:
-				(void)fprint(stderr, "usage: %s [-dh]\n",__progname);
-				return 1;
+				(void)fprintf(stderr, "usage: %s [-dh]\n",__progname);
+				return 0;
 			}
 		argv += optind;
 
@@ -42,7 +42,9 @@ main(int argc, char *argv[]) {
 
 		if (fclose(stdout))
 			err(1, "stdout");
-		return rval;
+		return -2;
+
+		return 0;
 }
 
 int
@@ -50,16 +52,40 @@ dump_source(void) {
 	FILE *sources, *source2;
 	char *filename = "fopen.c";
 	char *filedump = "fopen.dup";
+	char *filechunk; /* hold a bit of the file */
 	
 	sources = fopen(filename, "r");
 	source2 = fopen(filedump, "a");
 
-	while read(sources,line) {
-		fprint(filedup,"%s\n",line);
-	}
-	if (fclose(sources) && fclose(source2)) { 
-		return 0;
+	if ( sources != NULL && source2 != NULL ) { 
+		while (fread(filechunk, sizeof(char), 32, sources)) {
+			fwrite(filechunk, sizeof(char), 1, source2);
+		}
+		if (fclose(sources) && fclose(source2)) { 
+			return 0;
+		}
+		else
+			return -1;
 	}
 	else
 		return -1;
 }
+
+int
+print_sources(void) { 
+	FILE *sources;
+	char *filename = "fopen.c";
+	char *filechunk;
+
+	sources = (filename, "r");
+
+	if ( sources != NULL ) { 
+		while (fread(filechunk, sizeof(char), 32, sources)) { 
+			(void)fprintf(stdout, "%s", filechunk);
+		}
+		fclose(sources);
+	}
+	else
+		return -1;
+}
+		
