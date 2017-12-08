b4036019c57344153a601cd84ef85ab11c06ddca
newnix@exile.digital <newnix@exile.digital>
2017-12-08 00:08:12 +0000

    Revising History
    * ex123.c
    	This file apparently never worked. I've since 
    	learned a few new tricks and am rewriting it. 
    	I haven't finished yet because I need to go 
    	home, but it should be pretty simple.

--- c/kr/s1/ex123.c
+++ c/kr/s1/ex123.c
@@ -4,43 +4,46 @@
  * Meant to remove comments from a given C file
  */
 
-#define MAX 100000
-
-/* I don't have any experience reading/writing files, so I'll settle for stripping comments from stdin */
 #include <stdio.h>
-#include "../headers/get_line.h"
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-/* creating a custom version of the get_line function to allow it to continue past a single newline */
-int get_line(char s[], int max);
+#define BUFSIZE 64
+#define COMMENT 0
 
-int main()
-{
-	int len; /* length of the input string given by the user */
-	char instr[MAX]; /* input string pulled from user */
-	char lines[MAX][MAX]; /* intended to hold up to MAX lines of input from instr */
-	int linelen[MAX]; /* separate value to hold the length of each string stored */
-	int linenum; /* current line number we're on */
-	int step; /* iterator */
+void run_help(void);
+int strip_file(char * source);
 
-	/* take lines from stdin and strip out comments matching the format used here */
-	step = len = linenum = 0;
-	while ((len = get_line(instr, MAX)) > 0)
-	{
-		/* grab all the strings and track them in the array lines[] */
-		lines[linenum][0] = instr; /* store the current line in lines[linenum] */
-		linelen[linenum] = len; /* for the same position in linelen[] keep lines[] length */
-		linenum++;
-		printf("instr: %slines[%d]: %s\n", instr, (linenum - 1), lines[linenum - 1]);
+int
+main(int argc, char **argv) {
+	int block; /* are we in a comment block? */
+	int i, j; /* index values */
+	size_t size; /* size of the buffer */
+	char *text, *source, ch; /* strings for tracking text, and user defined file */
+
+	block = i = j = 0;
+	size = BUFSIZE;
+	if ((text = calloc(size, sizeof(char))) == NULL) {
+		fprintf(stderr,"Couldn't allocate space for `text`\n");
+		return 1;
 	}
-	/* so now we need to do things, like looping through the arrays and getting rid of comments */
-	/* first, let's prove that I stored the information correctly */
-	for (step = 0; step < linenum; step++)
-	{
-		printf("lines[%d]: %s Length:%d\n",step,lines[step],linelen[step]);
+	if ((source = calloc(size, sizeof(char))) == NULL) {
+		fprintf(stderr,"Couldn't allocate space for `source`\n");
 	}
-	free (instr);
-	free (lines);
-	free (linelen);
-	return 0;
+	
+	while ((ch = getopt(argc, argv, "f:h")) != -1) {
+		switch (ch) {
+			case 'f':
+				strncpy(source, optarg, size);
+				break;
+			case 'h':
+				run_help();
+				return 0;
+			default:
+				run_help();
+				break;
+		}
+	}
+	return strip_file(source);
 }
