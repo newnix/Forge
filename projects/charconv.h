e037bd9591fa2fd1f55fcecb5596ae249fb9a11e
newnix@exile.digital <newnix@exile.digital>
2017-12-18 16:09:12 +0000

    Added New Project Directories
    * nxi/
    	- Project to build a new interactive shell, 
    	  meant to be similar to rc from Plan9, but also
    		relatively simple to use if familiar with bash.
    * projects/
    	- New home of the projects database management program.
    * rsnix/
    	- New home of the RSS reader/podcatcher project. 
    
    These changes are meant to logically separate the files being 
    used for various projects, making it easier to keep track of which 
    belong to which project. Instead of turning c/generic into a 
    soup of source code.

---
+++ projects/charconv.h
@@ -0,0 +1,121 @@
+#ifndef CHARCONV_H
+#define CHARCONV_H
+/*
+ * I guess this is essentially becoming a prototype library.
+ * which is why the name changed from upper.h to charconv.h,
+ * Any time I learn a new way to manipulate characters in a string
+ * I'll add a function to this file, so it can be reused and tweaked as needed
+ */
+#include <string.h>
+/* if tabstop hasn't been defined, make it 4 */
+#ifndef TABSTOP
+#define TABSTOP 4
+/* this is necessary per #ifndef preprocessor directive */
+#endif
+
+/* convert characters to uppercase */
+/* this version only works on one char at a time */
+char upperc(char c)
+{
+	int upperdiff; /* difference between 'a' and 'A' */
+	upperdiff = (97 - 65);
+	/* checks if the value is between 'z' and 'a', then adjust if needed */
+	c = ((c <= 'z') && (c >= 'a')) ? (c - upperdiff) : c;
+	return c;
+}
+
+/* converts all chars in a string to uppercase */
+void upper(char s[])
+{
+	int i;
+	int upperdiff;
+	upperdiff = (97 - 65);
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		/* turns out ternary is pretty simple and fun */
+		s[i] = ((s[i] >= 'a') && (s[i] <= 'z')) ? (s[i] - upperdiff) : s[i];
+	}
+}
+
+/* converts all chars in a string to lowercase */
+void lower(char s[])
+{
+	int i;
+	int upperdiff;
+	upperdiff = (97 - 65);
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		s[i] = ((s[i] >= 'A') && (s[i] <= 'Z')) ? (s[i] + upperdiff) : s[i];
+	}
+}
+
+/* convert a single char to lowercase */
+char lowerc(char c)
+{
+	int upperdiff;
+	upperdiff = (97 - 65);
+
+	c = ((c <= 'Z') && (c >= 'A')) ? (c + upperdiff) : c;
+	return c;
+}
+
+/* 
+ * Yanked from ex302.c
+ * may not actually be useful in the real world
+ */
+void escape(char to[], char from[])
+{
+  /* 
+   * should be a for loop ending at the null terminator,
+   * case statement should escape after modifying the string and updating the index
+   * afterwards, the string should be appended a null terminator so it can be printed properly
+   */
+
+	int i, j; /* indexes for each string */
+
+	i = j = 0;
+
+	/* now we step through the strings */
+	for (i = 0; from[i] != 0; i++) /* '\0' is the same as 0 */
+	{
+		switch (from[i]) /* make decisions based off the value of t[i] */
+		{
+			case '\t':
+				/* found a tab, replace with literal \t */
+				to[j] = '\\';
+				j++;
+				to[j] = 't';
+				break;
+			case '\n':
+				/* found a newline, replace with literal \n */
+				to[j] = '\\';
+				j++; 
+				to[j] = 'n';
+				break;
+			default:
+				/* nothing special, just assign the value directly to the new string */
+				to[j] = from[i]; /* can't use i as the index for both, as they may not be the same value at this point */
+				break;
+		}
+		j++; /* increment every time we move to a new index in t[] */
+	}
+	to[j + 1] = 0; /* make sure we have a NULL terminator */
+}
+
+/*
+ * reversal function, reverses a string in place
+ */
+void reverse(char s[])
+{
+	int c, i, j;
+
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
+#endif
---
+++ rsnix/rsnix.c
@@ -0,0 +1,233 @@
+/*  Copyright (c) 2017, newnix
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the disclaimer
+ *  below) provided that the following conditions are met:
+ *  
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  
+ *  * Neither the name of the copyright holder nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ *  
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ *  LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ *  DAMAGE.
+ */ 
+
+#include <assert.h>
+/*
+ * Libxml2 includes need to
+ * go here
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <curl/curl.h>
+
+extern char *__progname;
+
+/*
+ * This tool is meant to be a command-line unitility to subscribe to and download RSS feeds,
+ * Ideally statically linked, and possibly even multithreaded to run multiple operations at once.
+ * This should be possible to run in an automated fashion as well as interactively, so users can modify some settings.
+ * This project is using the port www/rssroll as reference on how to work with this sort of data.
+ */
+
+/* 
+ * Set of macros that create more easily remembered than their hex values
+ */
+
+#define ADD 1
+#define DOWNLOAD 2
+#define LIST 4
+#define SETDIR 8
+#define SETLABEL 16
+#define UPDATE 32
+#define SETURL 64
+#define REMOVE 128
+#define CHANGE 256
+
+int display_subs(void);
+void fetch_subs(void);
+void cook_args(unsigned short int flags, char *directory, char *label, char *url);
+int update_subs();
+int add_sub(char *sub_name, char *sub_url);
+int rm_sub(int feed_id);
+int change_sub(int feed_id, char *old_name, char *new_name);
+int build_dlq(int feed_ids[]);
+int save_files(FILE *filename, char *dl_link);
+int print_help(void);
+
+int 
+main(int argc, char *argv[]) {
+	/* Get things started */
+	char directory[1024]; /* -d flag */
+	char label[1024]; /* -l flag */
+	char url[1024]; /* -U flag */
+	char ch; /* flag options */
+	unsigned int flags; /* 0000 0000 */
+	int i;
+
+  flags = 0;
+	while ((ch = getopt(argc, argv, "hdD:lL:uU:acr")) != -1) {
+		switch (ch){
+			case 'h': /* ask for help */
+				return(print_help());
+			case 'a': /* add an entry */
+				flags ^= ADD;
+				break;
+      case 'c': /* modify an existing entry in the table */
+        flags ^= CHANGE;
+        printf("Current value of flags: %G, %u\n",flags,flags);
+        break;
+			case 'd': /* download new entries */
+				flags ^= DOWNLOAD;
+				break;
+			case 'l': /* list currently tracked rss feeds */
+				flags ^= LIST;
+			case 'D': /* specify a download derictory */
+				flags ^= SETDIR;
+				assert(strlcpy(directory, optarg, 1024) < 1025);
+				break;
+			case 'L': /* set a label for the entry */
+				flags ^= SETLABEL;
+				assert(strlcpy(label, optarg, 1024) < 1025);
+				break;
+			case 'u': /* update feeds */
+				flags ^= UPDATE;
+				break;
+			case 'U': /* the url of the feed */
+				flags ^= SETURL;
+				assert(strlcpy(url, optarg, 1024) < 1025);
+				break;
+			case 'r': /* remove a feed */
+				flags ^= REMOVE;
+				break;
+			default:
+				break;
+		}
+	}
+
+	cook_args(flags, directory, label, url);
+	return 0;
+}
+
+void
+cook_args(unsigned short int flags, char *directory, char *label, char *url){
+	unsigned short int comp; /* simple way of tracking values for bitmap comparisons */
+	/* use the info with flags to determine what needs to be done */
+  /* the only incompatible flags are -r && -a, as well as the modification command -C */
+  if ((flags & ADD ) && (flags & REMOVE)){
+    (void)fprintf(stderr,"%s: -a, -r, and -c are mutually exclusive flags\n",__progname);
+    exit(1);
+  }
+  if ((flags & ADD) && (flags & CHANGE)){
+    (void)fprintf(stderr,"%s: -a, -r, and -c are mutually exclusive flags\n",__progname);
+    exit(1);
+  }
+  if ((flags & REMOVE) && (flags & CHANGE)){
+    (void)fprintf(stderr,"%s: -a, -r, and -c are mutually exclusive flags\n",__progname);
+    exit(1);
+  }
+	
+}
+
+int
+print_help(){
+	/* print the helpful message for the users */
+	(void)fprintf(stdout,"%s:\t Track and download RSS feeds.\n",__progname);
+	(void)fprintf(stdout,"\t-a\tAdd a new RSS entry\n");
+	(void)fprintf(stdout,"\t-d\tDownload RSS updates\n");
+	(void)fprintf(stdout,"\t-l\tList currently tracked RSS feeds\n");
+	(void)fprintf(stdout,"\t-u\tUpdate tracked RSS feeds\n");
+	(void)fprintf(stdout,"\t-r\tRemove a tracked RSS feed\n");
+	(void)fprintf(stdout,"\t-D\tDownload new entries to the given directory\n");
+	(void)fprintf(stdout,"\t-L\tGive an entry a short label, for ease of use\n");
+	(void)fprintf(stdout,"\t-U\tSet the RSS entry's URL\n");
+	(void)fprintf(stdout,"\n");
+	return 0;
+}
+
+void
+fetch_subs(){ 
+	/* some libcurl stuff goes here to run through the tracked feeds and download episodes */
+	/* each subscription should be able to go in its own directory for the sake of users organization preferences */
+}
+
+int
+update_subs(){
+	/* this should be handed off to libcurl to download new manifests of rss feeds */
+}
+
+int
+add_sub(char *sub_name, char *sub_url){
+	/* this should get some inforamation from the user in an interactive fashion to populate the subscription table */
+	/*
+	 * Essentially:
+	 * insert into subscriptions (title, label, url, directory) values ();
+	 */
+}
+
+int
+rm_sub(int feed_id){
+	/* interactively remove a sub from the table */
+	/*
+	 * Essentially:
+	 * drop ${target} from subscriptions;
+	 */
+}
+
+int
+display_subs(){
+	/* show the user what feeds they've subscribed to */
+	/* 
+	 * Essentially: 
+	 * select title, label, episodes, new from subscriptions;
+	 */
+}
+
+int
+change_sub(int feed_id, char *old_name, char *new_name){
+	/* Interactively allow the user to change the data in the database */
+	/* once values are taken, essentially run an update on the database */
+}
+
+int
+build_dlq(int *feed_ids){
+	/* this will compile a list of downloads for the given list of feds to work with */
+	/* should return the number of downloads completed */
+}
+
+int
+save_files(FILE *filename, char *dl_link){
+	/* this is meant to be called by build_dlq(), ideally in multiple threads or processes to maximize bandwidth utilization */
+}
+
+void
+interactive_mode(){
+	/* 
+	 * This should basically act as a looped main(), 
+	 * allowing the user to go through different operations without exiting
+	 * This is only meant to run if there were no flags specified on the command-line.
+	 * This may eventually be modified to use ncurses instead of raw text 
+	 */
+}
