ac555dbbbb5359f615343677351bd9d2f7e2b5dd
newnix@exile.digital <newnix@exile.digital>
2018-01-10 22:30:09 +0000

    Minor Updates to Envlist
    	* c/generic/envlist.c
    		- Added some functionality to pull data via getenv().

--- c/generic/envlist.c
+++ c/generic/envlist.c
@@ -5,13 +5,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
 
 extern char **environ;
 
 int
 main(void) {
 	int envp;
+	unsigned int uid, euid;
+	char *homedir, *username;
 
+	uid = euid = 0;
+	uid = getuid();
+	euid = geteuid();
+	homedir = getenv("HOME");
+	username = getenv("USER");
+
+	fprintf(stdout, "Reading environmental data for UID: %u, EUID: %u...\n",uid, euid);
+	fprintf(stdout, "Username: %s\n$HOME: %s\n",username, homedir);
 	for (envp = 0; environ[envp] != NULL; envp++) {
 		fprintf(stdout, "%p\t%s\n", environ[envp], environ[envp]);
 	}
