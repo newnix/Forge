78572be77332e5f8110870517fe1c5a5a098b95e
newnix@exile.digital <newnix@exile.digital>
2018-01-30 03:52:15 +0000

    Extending Envlist
      * envlist.c
        - Added some additional functionality to envlist.c, though
          this seems to have some issues somewhere in the argument parsing,
          as it hangs when presented with the '-l' flag for some reason.
          Will need to run through lldb/gdb at some point to see what's
          going on there

--- c/generic/envlist.c
+++ c/generic/envlist.c
@@ -5,16 +5,90 @@
  * Literally a reimplemented env(1), with fewer features
  */
 
+#include <sys/types.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-#include <sys/types.h>
 
 extern char **environ;
 
+/* some prototypes */
+void envwalk(void);
+void run_help(void);
+int senv(char *variable, char *value);
+int delenv(char *env);
+
 int
-main(void) {
+main(int argc, char **argv) {
+	int list, set, unset, ch;
+	char *env;
+	char *variable, *value;
+
+	ch = list = set = unset = 0;
+	
+	while ((ch = getopt(argc, argv, "d:hls:v:")) != 1) {
+		switch (ch) {
+			case 'd':
+				unset = 1;
+				env = calloc(65535,1);
+				strncpy(env, optarg, 65535);
+				break;
+			case 'h':
+				run_help();
+				return(0);
+			case 'l':
+				list = 1;
+				break;
+			case 's':
+				set = 1;
+				variable = calloc(65535,1);
+				strncpy(variable,optarg,65535);
+				break;
+			case 'v':
+				if (set == 0) {
+					break;
+				} else {
+					value = calloc(65535,1);
+					strncpy(value,optarg,65535);
+					break;
+				}
+			default:
+				break;
+		}
+	}
+	if (set == 1) {
+		senv(variable,value);
+	}
+	if (unset == 1) {
+		delenv(env);
+	}
+	if (list == 1) {
+		envwalk();
+	}
+	return(0);
+}
+
+/* create new setenv() functions */
+int
+delenv(char *env) {
+	return(unsetenv(env));
+}
+
+void
+run_help(void) {
+	fprintf(stdout,"basic reimplementation of the env(1) utility\n");
+}
+
+int
+senv(char *variable, char *value) {
+	return(setenv(variable, value, 1));
+}	
+
+void
+envwalk(void) {
 	int envp;
 	unsigned int uid, euid;
 	char *homedir, *username;
@@ -39,7 +113,4 @@ main(void) {
 	 * a 32-bit machine, I would expect sizeof(environ) to return 4.
 	 */
 	fprintf(stdout,"\nRead %d environmental variables\n", envp);
-	return(0);
 }
-
-/* create new setenv() functions */
