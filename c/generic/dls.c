0e938e02cdca48cf6452c0424af9b46cd8acda0a
newnix@exile.digital <newnix@exile.digital>
2018-01-31 21:24:08 +0000

    Working ls(1) Reimplementation
      * dls.c
        - Fixed arguments passed to fts_open() in dls()
        - Added a debug switch to print more FTSENT struct information
        - Removed some fprintf() calls to display variable information
        - This is roughly as fast as ls(1), though it is nowhere near as
          feature rich, and may very well fail spectacularly in some
          unforseen ways. May actually build on top of this code for the nxu
          projecT

--- c/generic/dls.c
+++ c/generic/dls.c
@@ -16,10 +16,11 @@
 
 extern char **environ;
 extern char *__progname;
+int dbg;
 
 /* Function prototyping */
 void run_help(void);
-int dls(char *target);
+int dls(char **target);
 int scan_args(int argsize, char **arglist);
 int prntent(FTSENT *entry);
 
@@ -27,14 +28,18 @@ int
 main(int argc, char **argv) { 
 	int i, newline, ch;
 
+	dbg = 0;
 	i = ch = newline = 0;
 	/* here we need to determine what sort of flags are going to be used */
 	fprintf(stdout,"Argv list:\n");
 	for (i = 0; i < argc; i++) { 
 		fprintf(stdout,"argc[%d]: %s\n", i, argv[i]);
 	}
-	while ((ch = getopt(argc, argv, "h1")) != -1) {
+	while ((ch = getopt(argc, argv, "dh1")) != -1) {
 		switch(ch) {
+			case 'd':
+				dbg=1;
+				break;
 			case 'h':
 				run_help();
 				return(0);
@@ -50,35 +55,59 @@ main(int argc, char **argv) {
 }
 
 int
-dls(char *target) {
+dls(char **target) {
 	FTS *ftsp;
 	FTSENT *entry;
-	fprintf(stdout,"Listing contents of %s\n:",target);
-	ftsp = fts_open(&target, FTS_COMFOLLOW | FTS_SEEDOT,0);
-	entry = fts_read(ftsp);
-	if (entry != NULL) {
-		prntent(entry);
-	} else { 
-		return(-1);
+	fprintf(stdout,"Listing contents of %s\n:",*target);
+	if ((ftsp = fts_open(target, FTS_PHYSICAL | FTS_COMFOLLOW | FTS_SEEDOT,0)) == NULL) {
+		err(1,"fts_open");
+	}
+
+	while ((entry = fts_read(ftsp)) != NULL) {
+		switch(entry->fts_info) {
+			case FTS_DC:
+				fprintf(stdout,"%s: Directory leads to cyclical tree!\n",entry->fts_name);
+				break;
+			case FTS_DNR:
+				err(1,"fts_read");
+				return(-1);
+			case FTS_ERR:
+				fprintf(stderr,"%s : %s",entry->fts_name,strerror(entry->fts_errno));
+				return(-1);
+			default:
+				prntent(entry);
+		}
 	}
 	return(0);
 }
 
 int
 prntent(FTSENT *entry) {
-	fprintf(stdout,"fts_info:    %d\n",entry->fts_info);
-	fprintf(stdout,"fts_accpath: %s\n",entry->fts_accpath);
-	fprintf(stdout,"fts_path:    %s\n",entry->fts_path);
-	fprintf(stdout,"filename:    %s\n",entry->fts_name);
-	fprintf(stdout,"fts_number:  %lld\n",entry->fts_number);
-	fprintf(stdout,"fts_pointer: %p\n",entry->fts_pointer);
-	return(0);
+	if (dbg) { 
+		if (entry->fts_level > 1) { 
+			return(0);
+		} else { 
+			fprintf(stdout,"fts_accpath: %s\n",entry->fts_accpath);
+			fprintf(stdout,"fts_path:    %s\n",entry->fts_path);
+			fprintf(stdout,"filename:    %s\n",entry->fts_name);
+			fprintf(stdout,"fts_level:  %ld\n",entry->fts_level);
+			return(0);
+		}
+	} else { 
+		if (entry->fts_level == 1) { 
+			fprintf(stdout,"%s\n",entry->fts_path);
+			return(0);
+		} else { 
+			return(0);
+		}
+	}
 }
 
 void
 run_help(void) { 
 	/* print out the usage info */
 	fprintf(stdout,"Minimal reimplementation of ls(1)\n");
+	fprintf(stdout,"\t-d\tDisplay debugging (struct) info\n");
 	fprintf(stdout,"\t-h\tThis text\n");
 	fprintf(stdout,"\t-1\tList one entry per line\n");
 }
@@ -89,15 +118,15 @@ scan_args(int argsize, char **arglist) {
 	
 	/* argv[0] is always *__progname */
 	for (i = 1; i < argsize; i++) { 
+	/*
 		fprintf(stdout,"arglist[%d] = %s\n",i, arglist[i]);
 		fprintf(stdout,"arglist[%d][0] = %c\n", i, arglist[i][0]);
+	 */
 		/* assume anything not proceeded by a '-' is a directory */
 		if (arglist[i][0] == '-') { 
 			/* do nothing */
 		} else { 
-			if (dls(arglist[i]) == -1) {
-				return(-1);
-			}
+			dls(&arglist[i]);
 		}
 	}
 	return(0);
