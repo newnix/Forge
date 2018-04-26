fde0cef4835d27e6aadc33d28fad919b23f45777
newnix@exile.digital <newnix@exile.digital>
2018-04-26 19:51:55 +0000

    Update IP
      * sysutil/ip.c
        - Parsing user input and generating an array for ip4 addresses
          is working
        - All informational functions (netmask, hosts, broadcast, network)
          need to be fleshed out
        - ip6 still needs to be implemented

--- sysutil/ip.c
+++ sysutil/ip.c
@@ -32,61 +32,60 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-#define IP4 0x0001
-#define IP6 0x0002
-#define DECMASK 0x0004
-#define HELP 0x0008
-#define HEXMASK 0x0010
-#define LISTHOSTS 0x0020
-#define OCTMASK 0x0040
+#define IP4 0x0020
+#define IP6 0x0040
+#define DECMASK 0x0001
+#define HELP 0x0002
+#define HEXMASK 0x0004
+#define LISTHOSTS 0x0008
+#define OCTMASK 0x0010
 #define IP4SEP '.'
 #define IP6SEP ':'
 
 char *__progname;
 
 int brdcast(uint8_t *addr);
+uint8_t * buildaddr(char *arg, int addrclass);
 int cook(uint8_t flags, int optind, char **argv);
 int decmask(uint8_t *addr);
 int hexmask(uint8_t *addr);
-int hostaddrs(uint8_t *addr);
+int hostaddrs(uint8_t *addr, int list);
 int netwkaddr(uint8_t *addr);
 int octmask(uint8_t *addr);
 void usage(void);
 
 int 
 main(int argc, char **argv) { 
-	uint8_t ip4[4];
-	uint8_t ip6[16];
-	uint8_t flags, mask;
+	uint8_t flags;
 	int ch;
 
-	*ip4 = 0;
-	*ip6 = 0;
-	ch = flags = mask = 0; 
+	ch = flags = 0; 
 
 	while ((ch = getopt(argc, argv, "46dhlox")) != -1) { 
+		/* *mask flags are mutually exclusive */
 		switch(ch) { 
-			case '4':
-				flags ^= IP4;
-				break;
-			case '6':
-				flags ^= IP6;
-				break;
 			case 'd':
+				/* 0000 0001 */
 				flags ^= DECMASK;
 				break;
 			case 'h':
+				/* 0000 0010 */
 				flags ^= HELP;
 				break;
 			case 'l':
+				/* 0100 0100 */
 				flags ^= LISTHOSTS;
 				break;
 			case 'o':
+				/* 0000 1000 */
 				flags ^= OCTMASK;
 				break;
 			case 'x':
+				/* 0001 0000 */
 				flags ^= HEXMASK;
 				break;
 			default:
@@ -96,9 +95,7 @@ main(int argc, char **argv) {
 	}
 
 	/* Parse the arguments */
-	cook(flags, optind, argv);
-
-	return(0);
+	return(cook(flags, optind, argv));
 }
 
 int
@@ -106,24 +103,173 @@ brdcast (uint8_t *addr) {
 	return(0);
 }
 
+uint8_t *
+buildaddr(char *arg, int addrclass) { 
+	static uint8_t addr[17];
+	int i,j,k;
+	char buf[5];
+
+	*addr = *buf = k = 0;
+
+	if (addrclass == IP4) { 
+		for (i = 0,j = 0; arg[i] != 0; i++) { 
+			if (arg[i] <= 57 && arg[i] >= 48) { 
+				buf[j] = arg[i];
+				j++;
+			} else if (arg[i] == '.') { 
+				buf[j++] = 0;
+				j ^= j;
+				addr[k] = (uint8_t)atoi(buf); 
+				k++;
+				memset(buf, 0, sizeof(buf));
+			} else if (arg[i] == '/') { 
+				/* this is where the subnet mask is set */
+				buf[j++] = 0;
+				addr[k] = atoi(buf);
+				j ^= j; /* reset j */
+				k++;
+				memset(buf, 0, sizeof(buf));
+				buf[0] = arg[i+1];
+				buf[1] = arg[i+2];
+				buf[3] = 0;
+				addr[k] = (uint8_t)atoi(buf);
+			}
+		}
+		fprintf(stderr,"%u.%u.%u.%u/%d\n",addr[0],addr[1],addr[2],addr[3],addr[4]);
+		return(addr);
+	} else if (addrclass == IP6) { 
+		return(addr);
+	}
+	return((uint8_t *) 1);
+}
+
 int 
 cook(uint8_t flags, int optind, char **argv) { 
+	/* Additional entry in the array is for the mask bits */
+	int addrclass, list;
+	uint8_t *ip;
+
+	addrclass = list = 0;
+	if ((ip = calloc(17, sizeof(uint8_t))) == NULL) { 
+		fprintf(stderr,"ERR: Failed to allocate memory!\n");
+		return(1);
+	}
+
 	if (flags == HELP || argv[1] == 0) { 
 		usage();
+		free(ip);
 		return(0);
+	} else { 
+		if (strchr(argv[optind], IP4SEP) == NULL && strchr(argv[optind], IP6SEP) == NULL) { 
+			fprintf(stderr,"%s: invalid IP address!\n",argv[optind]);
+			free(ip);
+			return(1);
+		} else { 
+			addrclass = ((strchr(argv[optind], IP4SEP) != NULL)) ? IP4 : IP6 ;
+			ip = buildaddr(argv[optind], addrclass);
+		}
+
+		fprintf(stderr,"flags = %u\n", flags);
+		switch(flags & 255) { 
+			/* at this point, we're only deciding which functions we're running */
+			case 0:
+				/* this should be the same as the default case, run all functions */
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				decmask(ip);
+				octmask(ip);
+				hexmask(ip);
+				break;
+			case DECMASK:
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				decmask(ip);
+				break;
+			case HEXMASK:
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				hexmask(ip);
+				break;
+			case LISTHOSTS:
+				list = 1;
+				break;
+			case OCTMASK:
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				octmask(ip);
+				break;
+			case DECMASK|LISTHOSTS:
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				decmask(ip);
+				list = 1;
+				break;
+			case HEXMASK|LISTHOSTS:
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				hexmask(ip);
+				list = 1;
+				break;
+			case OCTMASK|LISTHOSTS:
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				octmask(ip);
+				list = 1;
+				break;
+			default:
+				hostaddrs(ip,list);
+				brdcast(ip);
+				netwkaddr(ip);
+				decmask(ip);
+				octmask(ip);
+				hexmask(ip);
+				break;
+		}
 	}
+	/* free(ip); Not sure at the moment what happened, but this was causing an assertion failure */
+	return(0);
+}
+
+int
+decmask(uint8_t *addr) { 
+	return(0);
+}
+
+int
+hexmask(uint8_t *addr) { 
+	return(0);
+}
+
+int
+hostaddrs(uint8_t *addr, int list) { 
+	return(0);
+}
+
+int
+netwkaddr(uint8_t *addr) { 
+	return(0);
+}
+
+int
+octmask(uint8_t *addr) { 
 	return(0);
 }
 
 void
 usage(void) { 
 	fprintf(stdout,"%s: Simple IP address and netmask calculator\n",__progname);
-	fprintf(stdout,"\t-4\tExplicitly operate with the ip4 address space\n"
-								 "\t-6\tExplicitly operate with the ip6 address space\n"
-								 "\t-d\tOnly show the decimal representation of the netmask\n"
+	fprintf(stdout,"\t-d\tOnly show the decimal representation of the netmask\n"
 								 "\t-h\tThis help message\n"
 								 "\t-l\tList all possible host addresses, not just the range\n"
 								 "\t-o\tOnly show the octal representation of the netmask\n"
 								 "\t-x\tOnly show the hexadecimal representation of the netmask\n"
-								 "\n\tEx: ip 192.168.0.0/4\n");
+								 "\n\tEx: ip 192.168.0.0/4\n"
+								 "\t    ip fe80::6a05:caff:fe3f:a9da/64\n");
 }
