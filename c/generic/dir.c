f3f27da63962d3e9d88c55b8ebb09ad9fa81d17a
newnix@exile.digital <newnix@exile.digital>
2018-05-16 22:15:02 +0000

    Fixed Alignment in dir.c
      * c/generic/dir.c
        - Fixed alignment in printstat()

--- c/generic/dir.c
+++ c/generic/dir.c
@@ -45,15 +45,15 @@ main(int argc, char **argv) {
 static void
 printstat(struct stat *ent) {
 	fprintf(stdout,"inode:\t%lu\tlinks:\t%d\n"
-								 "device:\t%d\tmode:\t%03o\n"
-								 "uid:\t%d\tgid:\t%d\n"
-								 "dev type:\t%u\n"
-								 "size:\t%lu\tblocks:\t%ld\n"
-								 "blksize:\t%u\tflags:\t%u\n"
-								 "gen:\t%u\tlspare:\t%d\n"
-								 "qspare1:\t%ld\tqspare2:\t%ld\n",
-								 ent->st_ino,ent->st_nlink,ent->st_dev,ent->st_mode,
-								 ent->st_uid,ent->st_gid,ent->st_rdev,ent->st_size,
-								 ent->st_blocks,ent->st_blksize,ent->st_flags,ent->st_gen,
-								 ent->st_lspare,ent->st_qspare1,ent->st_qspare2);
+	               "device:\t%d\tmode:\t%03o\n"
+	               "uid:\t%d\tgid:\t%d\n"
+	               "dev type:\t%u\n"
+	               "size:\t%lu\tblocks:\t%ld\n"
+	               "blksize:\t%u\tflags:\t%u\n"
+	               "gen:\t%u\tlspare:\t%d\n"
+	               "qspare1:\t%ld\tqspare2:\t%ld\n",
+	               ent->st_ino,ent->st_nlink,ent->st_dev,ent->st_mode,
+	               ent->st_uid,ent->st_gid,ent->st_rdev,ent->st_size,
+	               ent->st_blocks,ent->st_blksize,ent->st_flags,ent->st_gen,
+	               ent->st_lspare,ent->st_qspare1,ent->st_qspare2);
 }
