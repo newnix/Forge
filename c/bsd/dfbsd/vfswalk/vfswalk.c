0520a2ba0dbdd149b536bf72e5f3ca94ed264295
newnix@exile.digital <newnix@exile.digital>
2018-06-21 18:10:48 +0000

    Finish vfswalk Implementation
      * c/bsd/dfbsd/vfswalk/vfswalk.c
        - Removed **environ, as it's not necessary for the functionality of
          this program
        - Minor formatting fix for clarity in the fsprint() and vfsprint()
          functions
        - Fixed vfswalk() to display filesystem information properly

--- c/bsd/dfbsd/vfswalk/vfswalk.c
+++ c/bsd/dfbsd/vfswalk/vfswalk.c
@@ -55,7 +55,6 @@
 #include <unistd.h>
 
 /* ensure the environment's accessible */
-extern char **environ;
 extern char *__progname;
 
 static void __attribute__((noreturn)) usage(void);
@@ -66,10 +65,6 @@ static void vfsprint(struct statvfs *vfsbuf);
 
 int
 main(int argc, char **argv) {
-	if (geteuid() != 0) { 
-		fprintf(stderr,"Warning, %s may require root privileges!\n",__progname);
-	}
-
 	if (argv[1] != NULL && strncmp(argv[1],"help",(size_t)5) == 0) { 
 		usage();
 	} 
@@ -100,12 +95,12 @@ fsprint(struct statfs *fsbuf) {
 							 "f_type: %d\tf_flags: %d\tf_syncwrites: %ld\n"
 							 "f_asyncwrites: %ld\tf_fstypename: %s\tf_mntonname: %s\n"
 							 "f_syncreads: %ld\tf_asyncreads: %ld\tf_mntfromname: %s\n\n",
-							 fsbuf->f_bsize,fsbuf->f_iosize,fsbuf->f_blocks,
-							 fsbuf->f_bfree,fsbuf->f_bavail,fsbuf->f_files,
-							 fsbuf->f_ffree,fsbuf->f_fsid.val[0],fsbuf->f_fsid.val[1],fsbuf->f_owner,
-							 fsbuf->f_type,fsbuf->f_flags,fsbuf->f_syncwrites,
-							 fsbuf->f_asyncwrites,fsbuf->f_fstypename,fsbuf->f_mntonname,
-							 fsbuf->f_syncreads,fsbuf->f_asyncreads,fsbuf->f_mntfromname);
+							 fsbuf->f_bsize, fsbuf->f_iosize, fsbuf->f_blocks, 
+							 fsbuf->f_bfree, fsbuf->f_bavail, fsbuf->f_files, 
+							 fsbuf->f_ffree, fsbuf->f_fsid.val[0], fsbuf->f_fsid.val[1], fsbuf->f_owner, 
+							 fsbuf->f_type, fsbuf->f_flags, fsbuf->f_syncwrites, 
+							 fsbuf->f_asyncwrites, fsbuf->f_fstypename, fsbuf->f_mntonname, 
+							 fsbuf->f_syncreads, fsbuf->f_asyncreads, fsbuf->f_mntfromname);
 }
 
 /*
@@ -153,20 +148,16 @@ vfsprint(struct statvfs *vfsbuf) {
 							 "f_flag: %lu\tf_namemax: %lu\tf_owner: %u\n"
 							 "f_type: %u\nf_syncreads: %lu\tf_syncwrites: %lu\n"
 							 "f_asyncreads: %lu\tf_asyncwrites: %lu\n",
-							 vfsbuf->f_bsize,vfsbuf->f_frsize,vfsbuf->f_blocks,vfsbuf->f_bfree,
-							 vfsbuf->f_bavail,vfsbuf->f_files,vfsbuf->f_ffree,vfsbuf->f_favail,
-							 vfsbuf->f_fsid,vfsbuf->f_flag,vfsbuf->f_namemax,vfsbuf->f_owner,
-							 vfsbuf->f_type,vfsbuf->f_syncreads,vfsbuf->f_syncwrites,vfsbuf->f_asyncreads,
+							 vfsbuf->f_bsize, vfsbuf->f_frsize, vfsbuf->f_blocks, vfsbuf->f_bfree, 
+							 vfsbuf->f_bavail, vfsbuf->f_files, vfsbuf->f_ffree, vfsbuf->f_favail, 
+							 vfsbuf->f_fsid, vfsbuf->f_flag, vfsbuf->f_namemax, vfsbuf->f_owner, 
+							 vfsbuf->f_type, vfsbuf->f_syncreads, vfsbuf->f_syncwrites, vfsbuf->f_asyncreads, 
 							 vfsbuf->f_asyncwrites);
 }
 
 /*
- * This should, if I'm reading the manual pages properly, return information on
- * ALL currently mounted filesystems, which will allow me to better investigate 
- * the struct values, though from the working vfsinfo() function, 
- * it looks like HAMMER2 filesystems are identified as fsbuf->f_type == 9
+ * Display info on every currently mounted filesystem
  */
-/* might need to look at getfsent(3) instead for this functionality */
 static int
 vfswalk(void) { 
 	struct statfs *fsbuf;
@@ -184,28 +175,20 @@ vfswalk(void) {
 		if ((fsbuf = calloc(sizeof(struct statfs *),fscount)) == NULL) {
 			return(-1);
 		}
-		if ((vfsbuf = calloc(sizeof(struct vfsstat *),fscount)) == NULL) { 
+
+		if ((vfsbuf = calloc(sizeof(struct statvfs *),1)) == NULL) {
 			return(-1);
 		}
-		vfsbufsize = (sizeof(*vfsbuf) * fscount);
+		vfsbufsize = (sizeof(*fsbuf) * fscount);
 	}
 	
-	if ((fscount = getfsstat(fsbuf,vfsbufsize,flags)) > 0) {
-		fprintf(stdout,"Attempting to print %d mounted filesystems...\n",fscount);
+	if ((fscount = getfsstat(fsbuf, vfsbufsize, flags)) != -1) { 
 		for (flags = 0; flags < fscount; flags++) {
+			fprintf(stdout,"\ninfo for %s:\n",fsbuf[flags].f_mntonname);
 			fsprint(&fsbuf[flags]);
-		}
-	}
-
-	flags = MNT_WAIT; /* just to be sure */
-
-	if ((fscount = getvfsstat(fsbuf, vfsbuf, vfsbufsize, flags)) != -1) { 
-		fprintf(stdout,"getvfsstat returned %d filesystems\n",fscount);
-		for (flags = 0; flags < fscount; flags++) {
-			fprintf(stdout,"vfsstat struct for %s:\n",fsbuf[flags].f_mntonname);
-			vfsprint(&vfsbuf[flags]);
-			fprintf(stdout,"\nfsstat struct:\n");
-			fsprint(&fsbuf[flags]);
+			if (statvfs(fsbuf[flags].f_mntonname,vfsbuf) == 0) {
+				vfsprint(vfsbuf);
+			}
 		}
 	} else {
 		err(errno,"getvfsstat: ");
