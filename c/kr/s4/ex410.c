57d90496ff3ecb204899e782983c2705d9224b3a
newnix <newnix>
2018-11-12 22:55:16 +0000

    More Work on EX4-10
    	* c/kr/s4/ex410.c
    		- Set main() to loop over input from getline(3)
    		- Defined both pop() and push()
    		- Created scan_input() to parse the input line, including an exit condition

--- c/kr/s4/ex410.c
+++ c/kr/s4/ex410.c
@@ -27,8 +27,10 @@ int bufp = 0;
 double pop(void);
 void push(double);
 void get_top(double pair[2]);
+int scan_input(char **line);
 int swap_top(double pair[2]);
 void clear_stack(void);
+void clear_line(void);
 
 int sp = 0;
 double val[MAXVAL];
@@ -36,19 +38,60 @@ double val[MAXVAL];
 int
 main(void) {
 	while (getline(&buf, (size_t *)BUFSIZE, stdin) != -1) {
+		/* 
+		 * Scan the input to determine what to do next
+		 */
+		switch(scan_input(&buf)) {
+			default:
+				clear_line();
+				clear_stack();
+				continue;
+		}
 	}
 }
 
 double
 pop(void) {
+	if (sp > 0) {
+		return val[--sp];
+	} else {
+		fprintf(stderr,"ERR: Stack empty\n");
+		return 0.0;
+	}
 }
 
 void
 push(double x) {
+	/* Push the given value on to the value stack */
+	if (sp < MAXVAL) { 
+		val[sp++] = x;
+	} else {
+		fprintf(stderr, "ERR: Stack full, unable to push %f\n", x);
+	}
 }
 
 void
 get_top(double pair[2]) {
+}
+
+int
+scan_input(char **line) {
+	int i;
+	
+	for (i = 0; (*line[i] != '\n') && (*line[i] != EOF); i++) {
+		if (isdigit(*line[i])) {
+			/* build the digit to be pushed */
+		}
+		if (strnstr(*line, "exit", (size_t)BUFSIZE) != NULL) {
+			/* 
+			 * since everything's statically allocated, simply bailing out should be fine
+			 * but I believe it's better to just return negative and have that handled in main()
+			 */
+			fprintf(stderr, "Quitting...\n");
+		}
+	}
+
+	return(0);
 }
 
 int
@@ -58,4 +101,9 @@ swap_top(double pair[2]) {
 void
 clear_stack(void) {
 	memset(&val, 0, sizeof(val));
+}
+
+void
+clear_line(void) {
+	memset(&buf, 0, sizeof(buf));
 }
