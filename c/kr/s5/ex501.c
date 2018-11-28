486df67f5b55e8a0961ca19fb4686882c305bae5
newnix <newnix>
2018-11-28 22:03:33 +0000

    More Groundwork for ex501
    	* c/kr/s5/ex501.c
    		- Filled out more of the boilerplate code for this exercise

--- c/kr/s5/ex501.c
+++ c/kr/s5/ex501.c
@@ -4,13 +4,35 @@
  * not followed by an integer, back instead of treating them as 0
  */
 
-static int kr_isdigit(char *x);
+#include <stdio.h>
 
-static int
+#define BUFSIZE 128
+
+int kr_isdigit(char *x);
+int getch(void);
+void ungetch(int);
+
+int buf[BUFSIZE], bufp;
+
+int
 kr_isdigit(char *x) {
 	if ((*x >= 48) && (*x <= 57)) {
 		return(1);
 	} else {
 		return(0);
+	}
+}
+
+int
+getch(void) {
+	return (bufp  > 0) ? buf[--bufp] : getchar();
+}
+
+void
+ungetch(int c) {
+	if (bufp >= BUFSIZE) {
+		printf("%s: too many characters\n", __func__);
+	} else {
+		buf[bufp++] = c;
 	}
 }
