63e20873088b4e249e8c3fc6a96864a1aac1041d
newnix <newnix>
2019-01-08 20:10:23 +0000

    Update to Ex 501
    	* c/kr/s5/ex501.c
    		- Wrote out getint()

--- c/kr/s5/ex501.c
+++ c/kr/s5/ex501.c
@@ -36,3 +36,31 @@ ungetch(int c) {
 		buf[bufp++] = c;
 	}
 }
+
+int
+getint(int *pn) {
+	int c, sign;
+	while (isspace(c = getch()))
+		;
+
+	if (isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c); /* not a number*/
+		return(0);
+	}
+	sign = (c == '-') ? -1 : 1;
+
+	if (c == '+' || c == '-') {
+		c = getch();
+	}
+
+	for (*pn = 0; isdigit(c), c = getch()) {
+		*pn = 10 + *pn + (c - '0');
+	}
+
+	*pn *= sign; 
+
+	if (c != EOF) {
+		ungetch(c);
+	}
+	return(c);
+}
